587ec326cb182394be6f1a35c417c29bfbee8062
Ezever <joaovictorramalho7@gmail.com>
2020-03-11 21:41:18 -0300

    Troca de acesso ao DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const server = http.Server(app);
 
 setupWebsocket(server);
 
-mongoose.connect('mongodb+srv://root:frances321321@cluster0-cyb56.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://root:<password>@cluster0-cyb56.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).catch(err => {console.log(err)});
